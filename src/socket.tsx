987f49998b6c668091bba42488d34b054fb3a539
Artu Reis <120587481+Artu-Dev@users.noreply.github.com>
2025-01-05 01:19:45 -0300

    Update socket.tsx

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // const URL =  process.env.NODE_ENV === 'production' ? undefined : 'https://onequestionserver.onrender.com/';
-const URL = 'https://onequestionserver.onrender.com/';
+const URL = 'https://one-question-server.vercel.app/';
 
 export const socket = io(URL, {
     auth: {
